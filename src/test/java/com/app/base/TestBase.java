260858ae94cb9633a34225f278b7c55422ff455f
Git1509 <vijaykumar.reddi1509@gmail.com>
2025-07-26 20:25:02 +0530

    first push

---
+++ src/test/java/com/app/base/TestBase.java
@@ -0,0 +1,13 @@
+package com.app.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestBase {
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("https://www.google.com/");
+
+	}
+}
